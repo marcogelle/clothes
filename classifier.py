faa224228b1c82ff42f109ede8eca4afe29e03af
Marco Gellecanao <marcogelle@berkeley.edu>
2020-09-01 20:06:11 -0700

    made model

--- classifier.py
+++ classifier.py
@@ -13,12 +13,9 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+model = keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28, 28)))
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dense(10))
+
+
